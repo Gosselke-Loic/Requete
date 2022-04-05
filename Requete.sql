4a44e80e3803b7f6ee6056d15a1cbf5f039fe36d
Gosselke Loic <99717440+Gosselke-Loic@users.noreply.github.com>
2022-04-05 17:02:42 +0200

    first commit

---
+++ Requete.sql
@@ -0,0 +1,24 @@
+DROP TABLE Armes;
+DROP TABLE Competences;
+DROP HeroeCompetences;
+DROP Heros;
+
+SELECT Nom FROM Heros
+	ORDER BY Nom ASC;
+
+SELECT PV FROM Heros
+	WHERE PV LIKE '40%';
+
+SELECT COUNT(ID), PV FROM Heros
+	WHERE PV LIKE '40%';
+
+SELECT * FROM Heros
+	ORDER BY PV ASC;
+
+SELECT PV, Nom FROM Heros
+	WHERE PV LIKE '50';
+
+SELECT SUM(PV) FROM Heros; 
+
+/* SELECT Heros.Nom, Competence.Type FROM Heros
+INNER JOIN Competence ON Competence.ID = Heros.ID; */
